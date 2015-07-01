c546feecd9cc42ee7e0f712347e173a807616128
Welkson Ramos <welksonr@hotmail.com>
2015-07-01 16:00:15 -0300

    Initial commit

---
+++ tmpl/default.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+*-------------------------------------------------------------------------------
+* DT Simple Youtube Embed - Embed Youtube videos in Joomla by Detalhar - http://detalharweb.com.br
+*-------------------------------------------------------------------------------
+* @package DT Simple Youtube Embed
+* @version 0.1.0
+* @author Detalhar http://http://detalharweb.com.br
+* @copyright (C) 2015 Detalhar. All Rights Reserved
+* @license - GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html or see LICENSE.txt
+*
+* DT Simple Youtube Embed is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+**/
+
+defined('_JEXEC') or die('Access Restricted');
+
